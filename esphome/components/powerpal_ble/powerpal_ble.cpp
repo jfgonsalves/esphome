846647ef9bda52b4184396844c94b8e3907d8239
Filipe Gonsalves <4081921+jfgonsalves@users.noreply.github.com>
2025-01-14 21:15:14 +1100

    Update powerpal_ble.cpp

--- esphome/components/powerpal_ble/powerpal_ble.cpp
+++ esphome/components/powerpal_ble/powerpal_ble.cpp
@@ -60,13 +60,14 @@ void Powerpal::parse_measurement_(const uint8_t *data, uint16_t length) {
     // float total_kwh_within_interval = pulses_within_interval / this->pulses_per_kwh_;
     float avg_watts_within_interval = pulses_within_interval * this->pulse_multiplier_;
     
-    ESP_LOGI(TAG, "Timestamp: %ld, Pulses: %d, Average Watts within interval: %f W, Daily Pulses: %d", unix_time, pulses_within_interval,
-             avg_watts_within_interval, daily_pulses_);
-
+    ESP_LOGI(TAG, "Timestamp: %lld, Pulses: %d, Average Watts within interval: %f W, Daily Pulses: %llu", 
+             unix_time, 
+             pulses_within_interval,
+             avg_watts_within_interval, 
+             daily_pulses_);
+    
     if (this->power_sensor_ != nullptr) {
       this->power_sensor_->publish_state(avg_watts_within_interval);
-      //
-    }
 
     if (this->cost_sensor_ != nullptr) {
       double mycost = (pulses_within_interval / this->pulses_per_kwh_) * this->energy_cost_;
